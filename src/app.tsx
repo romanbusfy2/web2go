5581ce18b88cc9e5d614d976f7d7ae592172be4b
Roman Busfy <busfy.roman@gmail.com>
2022-12-06 14:04:50 +0100

    init app

---
+++ src/app.tsx
@@ -0,0 +1,26 @@
+export function App() {
+  return (
+    <>
+      <a
+        href="https://www.linkedin.com/in/roman-busfy-798a9116b/"
+        target="_blank"
+      >
+        <img src="./vite.svg" alt="web2go" />
+      </a>
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+}
